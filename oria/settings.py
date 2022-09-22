4bff9d68c7eaf6012a042cf0c7812a5fca0d1f63
Johnny <johnametepeagboku@live.com>
2022-09-22 02:59:33 +0000

    added static files dir

--- oria/settings.py
+++ oria/settings.py
@@ -120,6 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 MEDIA_URL = '/mediafiles/'
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'oria/mediafiles_cdn')
 
