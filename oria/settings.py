84a9c697b35cfcdd4980bf0e4a1b18ae97b8f946
Johnny <johnametepeagboku@live.com>
2022-09-22 19:08:05 +0000

    fixing static files not found

--- oria/settings.py
+++ oria/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'agenceoria/static'),
+    os.path.join(BASE_DIR, 'static/'),
 )
 MEDIA_URL = '/mediafiles/'
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'oria/mediafiles_cdn')
